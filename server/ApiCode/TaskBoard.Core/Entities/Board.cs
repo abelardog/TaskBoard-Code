8286b6ea949b7cc452748a0c3e77ecad7c758770
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-06-19 20:09:39 -0300

    Creando entidades

---
+++ server/ApiCode/TaskBoard.Core/Entities/Board.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskBoard.Core.Entities
+{
+    internal class Board
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<List> Lists { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
---
+++ server/ApiCode/TaskBoard.Core/Entities/Card.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskBoard.Core.Entities
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public int StateId { get; set; }
+        public State State { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
