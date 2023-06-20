6a2a4f6f47afcc3ed0394ef8cd948ec2b81f19f9
lidioguedez <112365426+lidioguedez@users.noreply.github.com>
2023-06-20 14:24:24 -0300

    Agregando patrones de diseño de software

---
+++ server/ApiCode/TaskBoard.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskBoard.Core.Interfaces.Data;
+
+namespace TaskBoard.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
